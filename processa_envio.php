eecec82e1ff15ea54b98e0063975f43937dfd58e
caiqueoliveir4 <caiquepark@hotmail.com>
2021-02-11 13:38:28 -0300

    adicionando a biblioteca PHPMailer ao projeto

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,19 @@
-<?php 
- print_r($_POST);
+<?php
+// requerindo bibliotecas do PHPMailor 
+    require "bibliotecas/PHPMailer/Exception.php";
+    require "bibliotecas/PHPMailer/OAuth.php";
+    require "bibliotecas/PHPMailer/PHPMailer.php";
+    require "bibliotecas/PHPMailer/POP3.php";
+    require "bibliotecas/PHPMailer/SMTP.php";
+
+    // usando o "use" para estrair o recuros do namespace
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
+    
+  
+     
 
 
 // criando classe 
@@ -31,7 +45,7 @@
     // criar uma variavel e atribuir a ela uma  instancia dessa classe "Mensagem"
     $mensagem = new Mensagem();
     /* apartir da variavel "mensagem que isntancia meu objeto",
-    eu vou utiliar -> e na sequencia executar o metodo __set para setar os valores */
+    eu vou utiliar "->" e na sequencia executar o metodo __set para setar os valores */
     $mensagem-> __set('para', $_POST['para']);
     $mensagem-> __set('assunto', $_POST['assunto']);
     $mensagem->__set('mensagem', $_POST['mensagem']);
@@ -40,7 +54,46 @@
     /*agora podemos executar o metodo "mesagemValida() atravéz da variavel mensagem
      que armazena a intancia do objeto*/
 
-     $mensagem->mensagemValida();
+     If(!$mensagem->mensagemValida()) {
+        echo 'Mensagem não é valida';
+        die();
+     }
 
+     $mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.example.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     // SMTP username
+    $mail->Password   = 'secret';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 ?>
